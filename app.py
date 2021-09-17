a21b0be919d92d346c2ad079c310f7f5f000874f
abhis <abhishekanand038@gmail.com>
2021-09-17 16:19:07 +0530

    updated

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, url_for
+from src1.menu1 import menu1
+from src1.add_flower import adding
 
 app = Flask(__name__)
 
@@ -10,17 +12,27 @@ def welcome():
 
 @app.route('/home')
 def home():
-    return render_template('/home.html')
+    return render_template('home.html')
 
 
 @app.route('/menu')
 def menu():
-    return render_template('menu.html')
+    m = menu1
+    return render_template('menu.html', m=menu1)
 
 
 @app.route('/add_flower')
 def add_flower():
     return render_template('add_flower.html')
+
+
+@app.route('/add_flower', methods=["POST"])
+def get_flower():
+    f_code = request.form['f_code']
+    f_quantity = request.form['f_quantity']
+    f_name = request.form['f_name']
+
+    return render_template('pass.html', f_code=f_code, f_quantity=f_quantity, f_name=f_name)
 
 
 @app.route('/del_flower')
